a65349905415bd0bf2bfa14996438075d3de5a0f
Ehtasham Daud Satti <ehtashamdaudsatti@gmail.com>
2026-07-23 09:55:29 -0700

    Updated Semaphore Synchronizatio22n

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/semaphore"
+  baseURL: "https://semaphore-synchronization.vercel.app/api/semaphore"
 });
